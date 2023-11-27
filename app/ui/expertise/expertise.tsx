f0abcedeb6b05ba800a24226c23510b574a04391
dehelean-catalin <drcatalin00@gmail.com>
2023-11-27 23:40:46 +0200

    Fix expertise section

--- app/ui/expertise/expertise.tsx
+++ app/ui/expertise/expertise.tsx
@@ -2,19 +2,11 @@
 
 import { EXPERTISE } from "@/app/lib/data";
 import Image from "next/image";
-import { useRef } from "react";
 import styles from "./expertise.module.css";
 
 export default function Expertise() {
-	const sectionRef = useRef<HTMLDivElement>(null);
-	// useIntersectionObserver(sectionRef, "#expertise", {});
-
 	return (
-		<div
-			id="expertise"
-			ref={sectionRef}
-			className={`${styles.container} px-5 m-auto mb-20`}
-		>
+		<div id="expertise" className={`px-5 m-auto mb-20`}>
 			<h3 className="mb-14">What I do?</h3>
 			<div className="flex flex-wrap gap-10 m-auto md:gap-0">
 				{EXPERTISE.map((item, key) => (
