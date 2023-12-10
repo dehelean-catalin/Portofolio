2fd08b729bb4ea60744d5abbf1347a2e1a618922
dehelean-catalin <drcatalin00@gmail.com>
2023-12-10 13:05:06 +0200

    Expertise li

--- app/ui/home/home.tsx
+++ app/ui/home/home.tsx
@@ -7,26 +7,8 @@ import styles from "./home.module.css";
 
 export default function Home() {
 	const ref = useRef(null);
-	// const inView = useInView(ref, { once: true });
 
-	// const titleClassName = clsx(styles.title, {
-	// 	"opacity-0": !inView,
-	// 	"after delay-300": inView,
-	// });
-
-	// const descClassName = clsx({
-	// 	"opacity-0": !inView,
-	// 	"after": inView,
-	// });
-
-	// TODO: check delay for home items
-	// TODO: change color of the burger menu.
-	// TODO: ADD SOME THREE JS ANIMATIONS, ICON-LINK
-
-	// const resumeContainer = clsx("hidden md:block mt-3", {
-	// 	"initial-tab": !inView,
-	// 	"after delay-200": inView,
-	// });
+	// TODO: ADD SOME THREE JS ANIMATIONS, change expertise page
 
 	return (
 		<div ref={ref} id="home" className={`${styles["home-wrapper"]} px-5 mb-20`}>
@@ -34,13 +16,12 @@ export default function Home() {
 				<span>Software Developer</span>
 				<h1 className={styles.title}>Dehelean Catalin</h1>
 				<p className={styles.desc}>
-					I'm all about turning lines of code into something awesome. Whether
+					{`I'm all about turning lines of code into something awesome. Whether
 					it's crafting beautiful interfaces or building behind-the-scenes
 					magic, I'm here to make tech feel like a friendly conversation. Let's
 					create cool things together and sprinkle a bit of friendliness into
-					the world of coding!
+					the world of coding!`}
 				</p>
-				{/* <div className={resumeContainer}> */}
 
 				<LinkButton
 					href="/resume.pdf"
